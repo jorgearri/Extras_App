0d02426952127fdfd6394c449f969cba8a84aa02
jorgearri <jorgerdgez12@gmail.com>
2026-05-30 09:04:04 -0600

    Actualizacion de STORAGES para Django moderno

--- nucleo/settings.py
+++ nucleo/settings.py
@@ -119,6 +119,13 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 LOGIN_URL = '/'
